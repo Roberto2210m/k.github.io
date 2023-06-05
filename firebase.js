d73a8d401d9f848db6ca33a57880eea1ec96abc4
Roberto2210m <57074641+Roberto2210m@users.noreply.github.com>
2023-06-05 05:40:06 -0600

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,56 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  onSnapshot,
+  addDoc,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ9XeiANQv3oDBfF4GYWxuGDIEmbwiXxQ",
+    authDomain: "newagent-7bc4e.firebaseapp.com",
+    databaseURL: "https://newagent-7bc4e.firebaseio.com",
+    projectId: "newagent-7bc4e",
+    storageBucket: "newagent-7bc4e.appspot.com",
+    messagingSenderId: "633086385594",
+    appId: "1:633086385594:web:8003ccaf6e947dc0e15183"
+  // Put you credentials here
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
+
+/**
+ * Save a New Task in Firestore
+ * @param {string} title the title of the Task
+ * @param {string} description the description of the Task
+ */
+export const saveTask = (title, description) =>
+  addDoc(collection(db, "tasks"), { title, description });
+
+export const onGetTasks = (callback) =>
+  onSnapshot(collection(db, "tasks"), callback);
+
+/**
+ *
+ * @param {string} id Task ID
+ */
+export const deleteTask = (id) => deleteDoc(doc(db, "tasks", id));
+
+export const getTask = (id) => getDoc(doc(db, "tasks", id));
+
+export const updateTask = (id, newFields) =>
+  updateDoc(doc(db, "tasks", id), newFields);
+
+export const getTasks = () => getDocs(collection(db, "tasks"));
